7a721e9ff2e2f30dbf26f29eb38659c11663e48d
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-18 07:05:47 +0000

    Tuesday, April 18, 2023, 12:35 PM India Standard Time

--- 18 april 2023.sql
+++ 18 april 2023.sql
@@ -1,19 +1,20 @@
-declare
-num1 number(2);
-num2 number(2);
-begin
-num1 := :num1;
-num2 := :num2;
-if num1>num2 then
-goto p1;
-else
-goto p2;
-end if;
-<<p1>>
-dbms_output.put_line('num1 is greater than num2');
-<<p2>>
-dbms_output.put_line('num2 is greater than num1');
-end;
+DECLARENUM1 NUMBER(2);
+
+NUM2 NUMBER(2);
+
+BEGIN
+    NUM1 := :NUM1;
+    NUM2 := :NUM2;
+    IF NUM1>NUM2 THEN
+        GOTO P1;
+    ELSE
+        GOTO P2;
+    END IF;
+    <<P1>>
+    DBMS_OUTPUT.PUT_LINE('num1 is greater than num2');
+    <<P2>>
+    DBMS_OUTPUT.PUT_LINE('num2 is greater than num1');
+END;
 
 -----------------------------
 
